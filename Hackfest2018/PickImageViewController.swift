417909d407dd5e0610d495b0024ba3e4576fd9be
Samuli Tamminen <samuli.tamminen@vincit.com>
2018-11-03 16:33:15 +0200

    Disable camera from picker input

--- Hackfest2018/PickImageViewController.swift
+++ Hackfest2018/PickImageViewController.swift
@@ -40,36 +40,16 @@ class PickImageViewController: UIViewController {
     }
 
     func openImagePicker() {
-
         let picker = UIImagePickerController()
         picker.delegate = self
+        picker.sourceType = .photoLibrary
 
-        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { action in
-                picker.sourceType = .camera
-                self.present(picker, animated: true, completion: nil)
-            }))
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            picker.modalPresentationStyle = .popover
+            picker.popoverPresentationController?.sourceView = view
+            picker.popoverPresentationController?.sourceRect = pickImageButton.frame
         }
-
-        alert.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { action in
-            picker.sourceType = .photoLibrary
-            // on iPad we are required to present this as a popover
-            if UIDevice.current.userInterfaceIdiom == .pad {
-                picker.modalPresentationStyle = .popover
-                picker.popoverPresentationController?.sourceView = self.view
-                picker.popoverPresentationController?.sourceRect = self.pickImageButton.frame
-            }
-            self.present(picker, animated: true, completion: nil)
-        }))
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-
-        // on iPad this is a popover
-        alert.popoverPresentationController?.sourceView = self.view
-        alert.popoverPresentationController?.sourceRect = pickImageButton.frame
-
-        self.present(alert, animated: true, completion: nil)
+        present(picker, animated: true, completion: nil)
     }
 }
 
